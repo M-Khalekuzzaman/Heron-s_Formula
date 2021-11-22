8928fd03c36534d63f853c523dec6ea6c4e86e8f
Kaochar <94604055+M-Khalekuzzaman@users.noreply.github.com>
2021-11-22 12:28:22 +0600

    Add files via upload

---
+++ using heron's formula.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio.h>
+#include<math.h>
+using namespace std;
+int main()
+{
+    double side1,side2,side3,area,s;
+    cout<<"Input the length of 1st side of the triangle : ";
+    cin>>side1;
+    cout<<"Input the length of 2nd side of the triangle : ";
+    cin>>side2;
+    cout<<"Input the length of 3rd side of the triangle : ";
+    cin>>side3;
+
+    s = (side1+side2+side3)/2;
+    area = (s*(s-side1)*(s-side2)*(s-side3));
+    cout<<"The area of the triangle is : " <<sqrt(area)<<endl;
+
+    getch();
+}
